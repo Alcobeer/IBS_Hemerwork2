48b347b7f0b1a188e47b04fbac30080176a23d63
UinstonSad <mat_pas2xov@mail.ru>
2021-11-01 00:41:21 +0300

    Добавил библиотеку apache.commons  и перевёл вводивое значение в верхний регистр

--- src/main/java/src/ru/ibs/progect/homerwork2/App.java
+++ src/main/java/src/ru/ibs/progect/homerwork2/App.java
@@ -1,12 +1,13 @@
 //работу выполнил Пастухов Матвей г.Нижний Новгород
 package src.ru.ibs.progect.homerwork2;
 
+import org.apache.commons.lang3.StringUtils;
 import java.util.Scanner;
 
 public class App {
    public static void main(String[] string){
        Scanner in = new Scanner(System.in);
-       String hell= in.nextLine();
+       String hell= in.nextLine().toUpperCase();
        System.out.println(hell);
     }
 }
