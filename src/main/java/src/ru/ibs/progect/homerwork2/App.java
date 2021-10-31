dc5af2347e8c265393abf70ca7b8249770e646b1
UinstonSad <mat_pas2xov@mail.ru>
2021-10-31 19:15:33 +0300

    Всё починил!!! Теерь вводимая строка выводится из консоли

--- src/main/java/src/ru/ibs/progect/homerwork2/App.java
+++ src/main/java/src/ru/ibs/progect/homerwork2/App.java
@@ -1,7 +1,11 @@
 package src.ru.ibs.progect.homerwork2;
 
+import java.util.Scanner;
+
 public class App {
    public static void main(String[] string){
-        System.out.println("Hello World !");
+       Scanner in = new Scanner(System.in);
+       String hell= in.nextLine();
+       System.out.println(hell);
     }
 }
